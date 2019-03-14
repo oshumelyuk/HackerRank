2b6ea69b237ca0f4b41c9e2905355f7acda46319
o.shumelyuk <o.shumelyuk@gmail.com>
2019-03-14 08:59:03 +0200

    solve ComplexNumberMultiply challenge

---
+++ LeetCode/ComplexNumberMultiply.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    static class ComplexNumberMultiply
+    {
+        public static string Solve(string a, string b)
+        {
+            var partsA = a.Split("+");
+            var partsB = b.Split("+");
+
+            var intA = int.Parse(partsA[0]);
+            var intB = int.Parse(partsB[0]);
+
+            var iPartA = int.Parse(partsA[1].Substring(0, partsA[1].Length - 1));
+            var iPartB = int.Parse(partsB[1].Substring(0, partsB[1].Length - 1));
+
+            var intResult = (intA * intB) + (iPartA * iPartB * -1);
+            var iResult = (intA * iPartB) + (intB * iPartA);
+
+            return $"{intResult}+{iResult}i";
+        }
+
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,28 +6,8 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var res = KeysAndRooms.Solve(new [] { new int[]{1,3 }, new int[] { 3, 0, 1}, new int[] { 2}, new int[] { 0 } });
+            var res = ComplexNumberMultiply.Solve("1+-1i", "1+-1i");
             Console.WriteLine(res);
-        }
-
-        private static TreeNode BuildTree()
-        {
-            var one = new TreeNode(1);
-            var two = new TreeNode(2);
-            var three = new TreeNode(3);
-            var four = new TreeNode(4);
-            var five = new TreeNode(5);
-            var six = new TreeNode(6);
-            var seven = new TreeNode(7);
-
-            one.left = two;
-            one.right = three;
-            two.left = four;
-            three.left = five;
-            three.right = six;
-            five.left = seven;
-
-            return one;
         }
     }
 }
