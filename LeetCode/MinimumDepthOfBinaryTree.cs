56c0ba48573c7ec3a2f563ae5a2c7d844c95d7ff
o.shumelyuk <o.shumelyuk@gmail.com>
2019-03-15 09:21:13 +0200

    solve MinimumDepthOfBinaryTree challenge

---
+++ LeetCode/MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    static class MinimumDepthOfBinaryTree
+    {
+        public static int Solve(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var pathes = new List<int>();
+            FindMinLength(root, pathes, 0);
+            return pathes.Min();
+        }
+
+        private static void FindMinLength(TreeNode root, List<int> pathes, int level)
+        {
+            level++;
+            if (root.left == null && root.right == null)
+            {
+                pathes.Add(level);
+                return;
+            }
+            if (root.left != null)
+            {
+                FindMinLength(root.left, pathes, level);
+            }
+            if (root.right != null)
+            {
+                FindMinLength(root.right, pathes, level);
+            }
+
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,8 +6,23 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var res = ComplexNumberMultiply.Solve("1+-1i", "1+-1i");
+            var res = MinimumDepthOfBinaryTree.Solve(BuildTree());
             Console.WriteLine(res);
+        }
+
+        private static TreeNode BuildTree()
+        {
+            var three = new TreeNode(3);
+            var nine = new TreeNode(9);
+            var twenty = new TreeNode(20);
+            var fifteen = new TreeNode(15);
+            var seven = new TreeNode(7);
+
+            three.left = nine;
+            three.right = twenty;
+            twenty.left = fifteen;
+            twenty.right = seven;
+            return three;
         }
     }
 }
